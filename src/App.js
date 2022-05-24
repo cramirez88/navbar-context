07e4d1191756e88947645a943313931dfaaf6ce0
Christian Ramirez <christian.r0602@gmail.com>
2022-05-24 10:44:09 -0500

    Form context and component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import MyContext from './context/MyContext';
 import NavBar from './components/NavBar'
 import React, {useState} from 'react';
+import Form from './components/Form';
 
 // Context.Provider is how we provide the context info from our context to the child
 // useContext is how we access our context in components
@@ -11,6 +12,7 @@ const [name, setName] = useState('Bob Smith')
   return (
     <MyContext.Provider value={{name, setName}}>
        <NavBar />
+       <Form />
     </MyContext.Provider>
    
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,18 @@
+import { useContext } from "react"
+import MyContext from "../context/MyContext"
 
+
+
+const Form = () => {
+  const {name, setName} = useContext(MyContext)
+  return (
+    <div>
+      <label>Your Name</label>
+      <input onChange={(e) => {setName(e.target.value)}} value={name}></input>
+    </div>
+  )
+}
+
+export default Form
 
 
